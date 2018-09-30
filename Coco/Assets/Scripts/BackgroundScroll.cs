f6b337100b98079651a047c878b8bc8ecc8b2e4f
Bizuwork Melesse <bmelesse@elifsis.com>
2018-09-29 20:10:17 -0700

    Disable background parallaxing after game over

--- Coco/Assets/Scripts/BackgroundScroll.cs
+++ Coco/Assets/Scripts/BackgroundScroll.cs
@@ -8,6 +8,7 @@ public class BackgroundScroll : MonoBehaviour
     public float speed = 0.5f;
     private void Update()
     {
+        if (GameManager.Instance.GameOver) { return; }
         Vector2 offset = new Vector2(Time.time * speed, 0);
         GetComponent<Renderer>().material.mainTextureOffset = offset;
     }
