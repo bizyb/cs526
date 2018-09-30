e7a2ba05b148b6425c12f6b78ee51c03f88705dc
Bizuwork Melesse <bmelesse@elifsis.com>
2018-09-29 20:08:14 -0700

    Enable background parallaxing
    Added custom background parallaxing. This is different from parallaxing prefabs

---
+++ Coco/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour
+{
+
+
+    public float speed = 0.5f;
+    private void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+
+}
