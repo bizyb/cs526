cec2cde93511c773e2f34a849db11e09dee978bb
Bizuwork Melesse <bmelesse@elifsis.com>
2018-10-07 15:29:42 -0700

    Add background music with looping

--- Coco/Assets/Scripts/AudioController.cs
+++ Coco/Assets/Scripts/AudioController.cs
@@ -8,11 +8,13 @@ public class AudioController : MonoBehaviour {
     public static AudioController Instance {get { return audioController;} }
 
     public AudioSource audioSource;
+    public AudioSource backgroundSource;
     public AudioClip scoreClip;
     public AudioClip deathClip;
     public AudioClip wingFlapClip;
     public AudioClip joystickClip;
     public AudioClip backgroundClip;
+    GameManager game;
 
     private void Awake()
     {
@@ -22,6 +24,32 @@ public class AudioController : MonoBehaviour {
         else {
             audioSource.mute = true;
         }
+    }
+    private void Update()
+    {
+        if (game.GameOver)
+        {
+            backgroundSource.Stop();
+            return;
+        }
+        if (!backgroundSource.isPlaying) {
+            backgroundSource.PlayOneShot(backgroundClip);
+        }
+
+
+
+    }
+    //private void OnEnable()
+    //{
+    //    InvokeRepeating("AudioOnGamePlay", 1f, 0.01f);
+    //}
+    //private void OnDisable()
+    //{
+    //    CancelInvoke();
+    //}
+    private void Start()
+    {
+        game = GameManager.Instance;
     }
     public void AudioOnScore() {
         audioSource.PlayOneShot(scoreClip);
@@ -39,7 +67,11 @@ public class AudioController : MonoBehaviour {
 
     }
 
-    public void AudioOnGamePlay() {
-
-    }
+    //public void AudioOnGamePlay() {
+    //    if (game.GameOver) {
+    //        audioSource.Stop();
+    //    }
+    //    if (audioSource.isPlaying(backgroundClip))
+    //    audioSource.pl
+    //}
 }
