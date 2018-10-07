c13b17b87a06f79af47b1884d714128ed54e3dd3
Bizuwork Melesse <bmelesse@elifsis.com>
2018-10-06 23:02:11 -0700

    Destroy individual obstacles up on click/touch

--- Coco/Assets/Scripts/TouchController.cs
+++ Coco/Assets/Scripts/TouchController.cs
@@ -1,71 +1,23 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
+public class TouchController : MonoBehaviour
+{
 
-public class TouchController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
-            //RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //Destroy(this.gameObject);
-            //Debug.Log("collider name: " + hit.collider.name);
-            if (Physics.Raycast(ray, out hit))
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
             {
-                BoxCollider bc = hit.collider as BoxCollider;
-                if (bc != null)
-                {
-                    Debug.Log("collider name: " + hit.collider.name);
-                    //Destroy(bc.gameObject);
-                }
-                //if (hit != null) {
-
-                //}
-               //;
+                Destroy(hit.collider.gameObject);
             }
-
-            //if (hit.collider != null)
-            //{
-            //    //Debug.Log("collider name: " + hit.collider.name);
-            //    if (hit.collider.gameObject == this.gameObject) Destroy(this.gameObject);
-            //}
         }
-        Debug.Log("Exiting TouchController update");
-
     }
 
-    /*
-  * If an obstacle has been touched, destroy it.
-  */
-    //void OnTouch()
-    //{
-
-    //    Debug.Log("Entering OnTouch");
-    //    Touch[] touches = Input.touches;
-    //    Debug.Log("touch length: " + touches.Length);
-    //    for (int i = 0; i < touches.Length; i++)
-    //    {
-    //        Ray ray = Camera.main.ScreenPointToRay(touches[i].position);
-    //        RaycastHit hit;
-    //        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-    //        {
-    //            Debug.Log("collider name: " + hit.collider.name);
-    //            if (hit.collider.name == "Asteroid")
-    //            {
-    //                Destroy(hit.collider.gameObject);
-
-    //            }
-    //        }
-    //    }
-    //    Debug.Log("Exiting OnTouch");
-    //}
 }
