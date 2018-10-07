95f13f8dbceec35b900c2ec41f977456b6bf8260
Bizuwork Melesse <bmelesse@elifsis.com>
2018-10-07 10:00:49 -0700

    Cleanup GameManager.cs
    Remove all comments, functions, and statements not being used anymore

--- Coco/Assets/Scripts/GameManager.cs
+++ Coco/Assets/Scripts/GameManager.cs
@@ -5,9 +5,6 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 
-
-// TODO: score refers to distance travel; final text should say distance travelled
-// instead of 'Score' and Highest Score should change to Longest Journey
 public class GameManager : MonoBehaviour
 {
 
@@ -28,16 +25,7 @@ public class GameManager : MonoBehaviour
     public GameObject currentScore;
     public GameObject backgroundOne;
    
-    //public GameObject backgroundThree;
-    //public GameObject backgroundFour;
-    //public GameObject backgroundFive;
     public GameObject groundOne;
-    //public GameObject groundTwo;
-    //public GameObject groundThree;
-    //public GameObject groundFour;
-    //public GameObject groundFive;
-    //public GameObject messageContainer;
-    //public Text message;
     public int startTime;
     public Text scoreText;
     private bool[] alerted = new bool[5];
@@ -80,9 +68,6 @@ public class GameManager : MonoBehaviour
     // Prefabs //
     [Header("Prefabs")]
     public GameObject astroidPrefab;
-    //public GameObject debrisPrefab;
-    //public GameObject explosionPrefab;
-    //public GameObject healthPickupPrefab;
 
     // Spawning Info //
     bool spawning = true;
@@ -95,16 +80,12 @@ public class GameManager : MonoBehaviour
 
     // Score //
     [Header("Score")]
-    //public Text scoreText;
-    //public Text finalScoreText;
     int score = 0;
     public int asteroidPoints = 50;
     public int debrisPoints = 10;
 
     // Game Over //
     [Header("Game Over")]
-    //public Image gameOverScreen;
-    //public Text gameOverText;
 
     public int asteroidHealth = 100;
     public int debrisHealth = 20;
@@ -172,12 +153,6 @@ public class GameManager : MonoBehaviour
         CountdownText.OnCountdownFinished += OnCountdownFinished;
 
 
-
-        // update distance travelled, i.e. the score, every 1 second
-
-        //SetPageState(PageState.Start);
-        //ResetObjects();
-        //InvokeRepeating("OnPlayerScored", 1f, 1f); // remve after debuggin and enable the one in OnCountDownFinished
         Debug.Log("Exiting OnEnable");
 
 
@@ -205,7 +180,6 @@ public class GameManager : MonoBehaviour
         score = 0;
         gameOver = false;
         startTime = 0; //(Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-        //InvokeRepeating("OnPlayerScored", 1f, 1f); //this is the right place, not OnEnable
         Debug.Log("Exiting OnCountdownFinished");
 
 
@@ -214,38 +188,9 @@ public class GameManager : MonoBehaviour
     }
    
 
-
-    /*
-     * Scoring is based on time travelled. For every second spent travelling, 
-     * increment the distance by MILES_PER_SEC. In addition, alert the player 
-     * at certain points in the game about an approaching completion of the 
-     * current leg of the race. Changes are made according to the given schedule.
-     * Alerts should be displayed before the end of the current leg and removed 
-     * right before the background change.
-     * 
-     * The schedule (timesteps):
-     * Leg1: 0-750 (Ghana-Algeria)
-     * Leg2: 750-1200 (Algeria-Spain)
-     * Leg3: 1200-1500 (Spain-France)
-     * Leg4: 1500-1700 (France-England)
-     * Leg5: 1700-1800 (England to the castle)
-     * 
-     * Alerts:
-     *  Alert1: 0-50: Leg 1: Ghana to Algeria
-     *  Alert2: 700-750: Approaching Leg2: Algeria to Spain
-     *  Alert3: 1150-1200: Approaching Leg 3: Spain to France
-     *  Alert4: 1450-1500: Approaching Leg 4: France to England
-     *  Alert5: 1650-1700: Approaching Leg 5: Destination
-     *  
-    */
     void OnPlayerScored()
     {
         Debug.Log("Entering OnPlayerScored");
-
-        //Debug.Log("game over: " + gameOver);
-
-        //string str = UnityEngine.StackTraceUtility.ExtractStackTrace();
-        //Debug.Log(str);
 
         if (gameOver) { return; }
         //int timeNow = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
@@ -253,98 +198,6 @@ public class GameManager : MonoBehaviour
 
         score += MILES_PER_SEC;
         scoreText.text = score.ToString();
-
-        // Alert the player 
-        //if (elapsed >= 0 && elapsed < 50 && !alertSent) {
-        //    AlertPlayer(Alerts.Alert1); 
-        //}
-        //else if (elapsed >= 700 && elapsed < 750 && !alertSent) {
-        //    AlertPlayer(Alerts.Alert2); 
-        //}
-        //else if (elapsed >= 1150 && elapsed < 1200 && !alertSent) {
-        //    AlertPlayer(Alerts.Alert3);
-        //}
-        //else if (elapsed >= 1450 && elapsed < 1500 && !alertSent) {
-        //    AlertPlayer(Alerts.Alert4);
-        //}
-        //else if (elapsed >= 1650 && !alertSent) {
-        //    AlertPlayer(Alerts.Alert5);
-        //}
-
-        //if (elapsed >= 0 && elapsed < 750 && !backgroundChanged) {
-        //    ChangeBackground(Background.Leg1);
-        //}
-        //else if (elapsed >= 750 && elapsed < 1200 && !backgroundChanged)
-        //{
-        //    ChangeBackground(Background.Leg2);
-        //}
-        //else if (elapsed >= 1200 && elapsed < 1500 && !backgroundChanged)
-        //{
-        //    ChangeBackground(Background.Leg3);
-        //}
-        //else if (elapsed >= 1500 && elapsed < 1700 && !backgroundChanged)
-        //{
-        //    ChangeBackground(Background.Leg4);
-        //}
-        //else if (elapsed >= 1700 && !backgroundChanged)
-        //{
-        //    ChangeBackground(Background.Leg5);
-        //}
-
-        // use boolean flags to avoid having to make multiple calls to the 
-        // functions
-        //if (elapsed >= 0 && elapsed < 5 && !alerted[0])
-        //{
-        //    AlertPlayer(Alerts.Alert1);
-
-        //}
-        //else if (elapsed >= 5 && elapsed < 10 && !alerted[1])
-        //{
-        //    AlertPlayer(Alerts.Alert2);
-
-        //}
-        //else if (elapsed >= 10 && elapsed < 15 && !alerted[2])
-        //{
-        //    AlertPlayer(Alerts.Alert3);
-           
-        //}
-        //else if (elapsed >= 15 && elapsed < 20 && !alerted[3])
-        //{
-        //    AlertPlayer(Alerts.Alert4);
-
-        //}
-        //else if (elapsed >= 20 && !alerted[4])
-        //{
-        //    AlertPlayer(Alerts.Alert5);
-           
-        //}
-
-        //if (elapsed >= 0 && elapsed < 5 && !bgChanged[0])
-        //{
-        //    ChangeBackground(Background.Leg1);
-
-        //}
-        //else if (elapsed >= 5 && elapsed < 10 && !bgChanged[1])
-        //{
-        //    ChangeBackground(Background.Leg2);
-           
-        //}
-        //else if (elapsed >= 10 && elapsed < 20 && !bgChanged[2])
-        //{
-        //    ChangeBackground(Background.Leg3);
-           
-        //}
-        //else if (elapsed >= 20 && elapsed < 25 && !bgChanged[3])
-        //{
-        //    ChangeBackground(Background.Leg4);
-           
-        //}
-        //else if (elapsed >= 25 && !bgChanged[4])
-        //{
-            //ChangeBackground(Background.Leg5);
-           
-        //}
-
         Debug.Log("Exiting OnPlayerScored");
 
     }
@@ -360,171 +213,22 @@ public class GameManager : MonoBehaviour
         {
             PlayerPrefs.SetInt("HighScore", score);
         }
-        //StartCoroutine("DelayedTransition");
-        //SetPageState(PageState.GameOver);
-        //CancelInvoke();
+      
         joystickPage.SetActive(false);
         gameOverScreen.gameObject.SetActive(true);
         Debug.Log("Exiting OnPlayerDied");
 
     }
 
-
-    //void AlertPlayer(Alerts alert) {
-    //    //TODO: play sound 
-
-    //    String s = "";
-    //    switch (alert) {
-    //        case Alerts.Alert1:
-    //            s = "Leg 1: Ghana to Algeria";
-    //            alerted[0] = true;
-    //            break;
-    //        case Alerts.Alert2:
-    //            s = "Approaching Leg2: Algeria to Spain";
-    //            alerted[1] = true;
-    //            break;
-    //        case Alerts.Alert3:
-    //            s = "Approaching Leg 3: Spain to France";
-    //            alerted[2] = true;
-    //            break;
-    //        case Alerts.Alert4:
-    //            s = "Approaching Leg 4: France to England";
-    //            alerted[3] = true;
-    //            break;
-    //        case Alerts.Alert5:
-    //            s = "Approaching Leg 5: Destination";
-    //            alerted[4] = true;
-    //            break;
-    //    }
-    //    messageContainer.SetActive(true);
-    //    message.text = s;
-
-    //}
-
     void ChangeBackground(Background leg) {
 
         switch (leg) {
             case Background.Leg1:
                 backgroundOne.SetActive(true);
-                //backgroundTwo.SetActive(false);
-                //backgroundThree.SetActive(false);
-                //backgroundFour.SetActive(false);
-                //backgroundFive.SetActive(false);
-
                 groundOne.SetActive(true);
-                //groundTwo.SetActive(false);
-                //groundThree.SetActive(false);
-                //groundFour.SetActive(false);
-                //groundFive.SetActive(false);
-
                 bgChanged[0] = true;
-                break;
-            //case Background.Leg2:
-                //backgroundOne.SetActive(false);
-                //backgroundTwo.SetActive(true);
-                //backgroundThree.SetActive(false);
-                //backgroundFour.SetActive(false);
-                //backgroundFive.SetActive(false);
-
-                //groundOne.SetActive(false);
-                //groundTwo.SetActive(true);
-                //groundThree.SetActive(false);
-                //groundFour.SetActive(false);
-                //groundFive.SetActive(false);
-
-                //bgChanged[1] = true;
-                //break;
-            //case Background.Leg3:
-            //    backgroundOne.SetActive(false);
-            //    backgroundTwo.SetActive(false);
-            //    backgroundThree.SetActive(true);
-            //    backgroundFour.SetActive(false);
-            //    backgroundFive.SetActive(false);
-
-            //    groundOne.SetActive(false);
-            //    groundTwo.SetActive(false);
-            //    groundThree.SetActive(true);
-            //    groundFour.SetActive(false);
-            //    groundFive.SetActive(false);
-
-            //    bgChanged[2] = true;
-            //    break;
-            //case Background.Leg4:
-            //    backgroundOne.SetActive(false);
-            //    backgroundTwo.SetActive(false);
-            //    backgroundThree.SetActive(false);
-            //    backgroundFour.SetActive(true);
-            //    backgroundFive.SetActive(false);
-
-            //    groundOne.SetActive(false);
-            //    groundTwo.SetActive(false);
-            //    groundThree.SetActive(false);
-            //    groundFour.SetActive(true);
-            //    groundFive.SetActive(false);
-
-            //    bgChanged[3] = true;
-            //    break;
-            //case Background.Leg5:
-                //backgroundOne.SetActive(false);
-                //backgroundTwo.SetActive(false);
-                //backgroundThree.SetActive(false);
-                //backgroundFour.SetActive(false);
-                //backgroundFive.SetActive(true);
-
-                //groundOne.SetActive(false);
-                //groundTwo.SetActive(false);
-                //groundThree.SetActive(false);
-                //groundFour.SetActive(false);
-                //groundFive.SetActive(true);
-
-                //bgChanged[4] = true;
-                //break;
+                break; 
         }
-        //if (gameOver) { return; }
-        //String msg = "";
-        //if (leg == Background.Leg2) { msg = "Leg2: Algeria to Spain"; }
-        //else if (leg == Background.Leg3) { msg = "Leg3: Spain to France"; }
-        //else if (leg == Background.Leg4) { msg = "Leg 4: France to England"; }
-        //else if (leg == Background.Leg5) { msg = "Leg 5: Destination"; }
-        //messageContainer.SetActive(true);
-        //message.text = msg;
-
-        //Debug.Log("game over state: " + gameOver);
-        //Debug.Log("setting message container to active....");
-
-    }
-
-    //IEnumerator OnGameOverSuccess()
-    //{
-    //    Debug.Log("Entering OnGameOverSuccess");
-    //    yield return new WaitForSeconds(3);
-       
-
-    //}
-
-    /*
-     * Clear the screen before displaying game over text.
-    */
-    IEnumerator DelayedTransition()
-    {
-
-        yield return new WaitForSeconds(0);
-        SetPageState(PageState.GameOver);
-
-        //if (finalLeg) {
-        //    // wait about 26 seconds to end the game; that's how long it takes
-        //    // for the castle to center
-
-        //    Debug.Log("returned from yielding...about to set success!!!");
-        //    SetPageState(PageState.GameOverSuccess);
-        //}
-        //else {
-        //    SetPageState(PageState.GameOver);
-        //}
-        healthBar.SetActive(false);
-        currentScore.SetActive(false);
-        //messageContainer.SetActive(false);
-        //message.text = "";
 
     }
 
@@ -636,12 +340,6 @@ public class GameManager : MonoBehaviour
         // Create the asteroid game object at the position( determined above ), and at a random rotation.
         GameObject ast = Instantiate(astroidPrefab, pos, Quaternion.Euler(0, 0, 0)) as GameObject;
 
-        // Call the setup function on the asteroid with the desired force and torque.
-        //ast.GetComponent<AsteroidController>().Setup(-pos.normalized * 1000.0f, Random.insideUnitSphere * Random.Range(500.0f, 1500.0f));
-
-        // Assign the health values to the asteroid.
-        //ast.GetComponent<AsteroidController>().health = asteroidHealth;
-        //ast.GetComponent<AsteroidController>().maxHealth = asteroidHealth;
     }
 
 
