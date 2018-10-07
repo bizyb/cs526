c00c0fc7da6b7b2561c1f13e9c02257b9d98dd2c
Bizuwork Melesse <bmelesse@elifsis.com>
2018-10-07 10:46:45 -0700

    Remove astroid scene

--- Coco/Assets/Scripts/Tap.cs
+++ Coco/Assets/Scripts/Tap.cs
@@ -51,7 +51,7 @@ public class Tap : MonoBehaviour
         isDead = false;
         anim.speed = 1;
         anim.SetTrigger("Flap");
-        upForce = 200f;
+        upForce = 100f;
         downForce = -200f; // the bird is a bit bouncy but it's fine
         //rigidbod.transform.position.x;
     }
@@ -78,42 +78,6 @@ public class Tap : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //if (game.GameOver) { return; }
-        //if (!isDead)
-        //{
-
-            //if (Input.GetMouseButtonDown(0))
-            //{
-            //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //    Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-
-            //    RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            //    if (hit.collider != null && hit.collider.gameObject == joystick)
-            //    {
-            //        Debug.Log("jostick clicked...");
-            //        //rigidbod.velocity = Vector2.zero;
-            //        //rigidbod.AddForce(new Vector2(0, upForce));
-            //        ////Debug.Log("current bird y: " + transform.position.y);
-            //        //anim.SetTrigger("Flap");
-            //    }
-            //    rigidbod.velocity = Vector2.zero;
-            //    rigidbod.AddForce(new Vector2(0, upForce));
-            //    //Debug.Log("current bird y: " + transform.position.y);
-            //    anim.SetTrigger("Flap");
-
-
-            //}
-        //}
-       
-        //if (game.backgroundFive.activeInHierarchy && !game.FinalLeg) {
-        //    game.FinalLeg = true;
-        //    StartCoroutine("OnGameOverSuccess");
-        //}
-
-    }
     public void OnJoystickTouch() {
         if (game.GameOver) { return; }
         if (!isDead)
