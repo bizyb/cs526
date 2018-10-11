2008891ba328ae3a6df2276f4c256df7b2461931
Bizuwork Melesse <bmelesse@elifsis.com>
2018-10-10 18:18:38 -0700

    Add animated eagle obstacle

--- Coco/Assets/Scripts/RandomPath.cs
+++ Coco/Assets/Scripts/RandomPath.cs
@@ -7,17 +7,24 @@ public class RandomPath : MonoBehaviour
 
     public GameManager game;
     private Transform target;
+    private Animator anim;
 
     // Speed in units per sec.
     public float speed = 1f;
     private void Start()
     {
         game = GameManager.Instance;
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (game.GameOver) { return; }
+       
+        if (game.GameOver) {
+            anim.SetTrigger("Idle");
+            return; 
+        }
+        anim.SetTrigger("Flap");
         target = game.bird.transform;
         // The step size is equal to speed times frame time.
         if (target != null)
